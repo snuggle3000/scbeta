21160a06a1b647c8e8a19c9fc80150e706c99cda
Shivam <shivam@snugglecloud.(none)>
2010-11-21 19:22:17 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SnuggleCloud::Application.config.secret_token = 'd1bfe551f1aa057dfd005b7e0b7f07d824e7221a9d60916a7a2fa5241b4bc59e51717b342774801672d788319cf0fae183449cf9c09efc96765c4dbf136b3421'
