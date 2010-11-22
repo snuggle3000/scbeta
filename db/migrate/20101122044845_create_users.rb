022461d9dd102cd4b360cdb7598a53bf363e85f8
David Mah <Mahhaha@gmail.com>
2010-11-21 21:34:29 -0800

    Migration was wrong lol

--- db/migrate/20101122044845_create_users.rb
+++ db/migrate/20101122044845_create_users.rb
@@ -1,11 +1,11 @@
 class CreateUsers < ActiveRecord::Migration
   def self.up
     create_table :users do |t|
-      t.String :email
-      t.String :first_name
-      t.String :last_name
-      t.String :hashed_password
-      t.String :other_email
+      t.string :email
+      t.string :first_name
+      t.string :last_name
+      t.string :hashed_password
+      t.string :other_email
 
       t.timestamps
     end
